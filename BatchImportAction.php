23c24108965bf776e2ebd8090f9789d6dab2e2be
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-11-22 15:04:49 +0300

    Initial

---
+++ BatchImportAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace yz\admin\import;
+
+use yii\base\Action;
+
+
+/**
+ * Class BatchImportAction provides base action that can be used for importing of the records from CSV files
+ */
+class BatchImportAction extends Action
+{
+
+} 
